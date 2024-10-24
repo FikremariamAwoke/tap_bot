c915a486887f4d85761a5e86ece8fb97845ff6f0
Fikremariam <fikrea197@gmail.com>
2024-10-24 21:35:17 +0300

    update: updated build script

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+IMAGE_NAME=urkocoin-bot
+
+# shellcheck disable=SC2181
+if [[ "$(docker images -q $IMAGE_NAME 2> /dev/null)" != "" ]]; then
+  echo "--> removing '$IMAGE_NAME'";
+  docker image rm -f $IMAGE_NAME;
+fi;
 
 # Build the Docker image
-docker build --no-cache -t urkocoin-bot .
+docker build --no-cache -t $IMAGE_NAME .
 
 # Run the Docker container
-docker run -d --name urkocoin-bot-container --cap-add=SYS_ADMIN urkocoin-bot
-
+docker run -d --name urkocoin-bot-container --cap-add=SYS_ADMIN $IMAGE_NAME
